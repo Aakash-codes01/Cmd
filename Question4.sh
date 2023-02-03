c813f9ede72d2bac15bd34f39ca01a7fd3ce36c8
Aakash <akash.s@sigmoianalytics.com>
2023-02-03 14:46:07 +0530

    Question4 completed and executed successfully

---
+++ Question4.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+
+mkdir Assignment
+cd Assignment
+touch File1.txt
+cat ../Table.sh > File1.txt
+echo -e "\nWelcome to sigmoid'" >> File1.txt
+echo "The contents of Desktop are"
+ls ~/Desktop 
