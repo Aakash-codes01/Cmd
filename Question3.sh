32a470502d67df23c04d20727cff3e692d3e410c
Aakash <akash.s@sigmoianalytics.com>
2023-02-08 13:12:34 +0530

    changes in question3

--- Question3.sh
+++ Question3.sh
@@ -1,17 +1,27 @@
 #!bin/bash
-echo "Enter the number you want to check"
-read number
 
-i=2
+function isprime
+{
+        echo "Enter the number you want to check"
+        read number
+        if [ $number -eq 1 ];
+        then 
+           echo "$((number )) is not a prime Number"
+           exit
+        fi
+        i=2
+        while [ $((i*i)) -le $number ];
+        do
+          if [ $((number%i)) -eq 0 ] ;
+          then
+            echo "$((number )) is not a prime Number"
+            exit
+          
+          fi
+          ((i++))
+        done
+        echo "$((number )) is a prime Number"
 
-while [ $((i*i)) -le $number ];
-do
-   if [ $((number%i)) -eq 0 ] ;
-   then
-     echo "$((number )) is not a prime Number"
-     exit
-   
-   fi
-   ((i++))
-done
-echo "$((number )) is a prime Number"
+}
+
+isprime
